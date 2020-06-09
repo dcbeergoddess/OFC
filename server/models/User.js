8e475372ddbff5520faf337a64972c0f0c1b435b
Bryan <bryan.fennell@hotmail.com>
2020-06-09 14:04:04 -0400

    Able to create accounts with hased passwords.

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const
+  mongoose = require('mongoose'),
+  bcrypt = require('bcryptjs')
+
+const
+  Schema = mongoose.Schema,
+  SALT_WORK_FACTOR = 10
+
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+UserSchema.pre('save', function(next) {
+  const user = this
+
+  if (!user.isModified('password')) return next()
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+
+module.exports = mongoose.model('User', UserSchema)
--- server/server.js
+++ server/server.js
@@ -1,7 +1,42 @@
-const express = require("express");
-const path = require("path");
+
+
+const
+  express = require("express"),
+  path = require("path"),
+  mongoose = require('mongoose')
+
+
+const User = require('./models/User')
+
+require('dotenv').config()
+
 const PORT = process.env.PORT || 3001;
+
+console.log(process.env.MONGODB_URI)
 const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI || '<to-do: public-uri>',
+  (error) => {
+    if (error) throw error
+    console.log('Successfully connected to MongoDB')
+  }
+)
+
+const test = new User({
+  username: 'alice',
+  password: 'Password123'
+})
+
+test.save(error => console.error(error))
+
+const test2 = new User({
+  username: 'bob',
+  password: 'Password123'
+})
+
+test2.save(error => console.error(error))
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
