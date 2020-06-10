491615d371bf1ca95c83f9fc66e78f74a9beb262
Rachel Murray <dcbeergoddess@gmail.com>
2020-06-10 12:07:15 -0400

    created json for history/timeline data and populated cards with no styling to history.js

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,14 +1,25 @@
 import React from "react";
 import {Link, Route} from "react-router-dom";
+import TimelineCard from "../components/TimelineCard"
+import timelineJson from "../timeline.json"
 import Stats from "./Stats"
 
 function History(props){
   return (
-    <div>
-      <h1>History of Change</h1>
-      <p>
-        I'm baby pitchfork hashtag waistcoat bitters fanny pack seitan disrupt cold-pressed slow-carb. Chia activated charcoal vaporware dreamcatcher hell of try-hard. Godard pop-up salvia hammock artisan fashion axe. Semiotics DIY photo booth normcore, austin direct trade tilde +1. Freegan cardigan you probably haven't heard of them disrupt. Quinoa pinterest gastropub, tofu tilde distillery meh fingerstache small batch cold-pressed truffaut disrupt photo booth.
-      </p>
+    <>
+      <h1>History of Systemic Inequality</h1>
+
+      <div>
+        {timelineJson.map(timeline => (
+          <TimelineCard 
+          key={timeline.id}
+          title={timeline.title}
+          date={timeline.date}
+          image={timeline.image}
+          text={timeline.text}
+          />
+        ))}
+      </div>
 
       <Link to={`${props.match.url}/stats`} role="button" className="btn btn-link">
         SHOW Stats
@@ -18,7 +29,7 @@ function History(props){
       </Link>
 
       <Route exact path={`${props.match.url}/stats`} component={Stats} />
-    </div>
+    </>
   );
 }
 
