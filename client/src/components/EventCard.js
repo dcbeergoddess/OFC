b09244c22b097251e581a09654b8ec0897d901ea
Claudia Petty <claudialove32@gmail.com>
2020-06-11 14:20:44 -0400

    continuing to try to get cards to align but also added shadow effect to headline text

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,27 +1,41 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./EventCard.css";
-import Flex from "./Flex";
 import "../index.css";
 
 
 class EventCard extends React.Component {
     render() {
         return (
-        <div className="flex-container">
-            <div className="row">
-        <div className="card">
-                <h2 className="name">{this.props.title}</h2>
-                <img src={this.props.image}
-                    alt={this.props.title} />
-                    <h3 >{this.props.date}</h3>
-                    <h3 >{this.props.time}</h3>
-                    <h3 >{this.props.location}</h3>
-                    <Link to="/EventDetail" role="button" className="btn btn-dark">See Details</Link>
-                 <Link  to={`/EventMain`} ></Link>
-                 </div>
-            </div>
-            </div>
+            <div class="container">
+                <header class="jumbotron">
+                        <h1> Get Involved!!</h1>
+                        <p> Browse events or leave comments on existing events</p>
+                </header>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Members have added the following events </h3>
+                    </div>
+                </div>
+
+                <div class="row text-center">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <div className="card">
+                                <h2 className="name">{this.props.title}</h2>
+                                <img src={this.props.image}
+                                    alt={this.props.title} />
+                                <h3 >{this.props.date}</h3>
+                                <h3 >{this.props.time}</h3>
+                                <h3 >{this.props.location}</h3>
+                                <Link to="/EventDetail" role="button" className="btn btn-dark">See Details</Link>
+                                <Link to={`/EventMain`} ></Link>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
         )
     }
 }
