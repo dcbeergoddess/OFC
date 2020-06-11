c600f8134fc383fbe50987a07f2542a0694d7e6f
Claudia Petty <claudialove32@gmail.com>
2020-06-11 13:16:42 -0400

    bug fix on image link in index.css

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./EventCard.css";
+import Flex from "./Flex";
+import "../index.css";
 
 
 class EventCard extends React.Component {
     render() {
-        return (<div className="card">
+        return (
+        <div className="flex-container">
+            <div className="row">
+        <div className="card">
                 <h2 className="name">{this.props.title}</h2>
                 <img src={this.props.image}
                     alt={this.props.title} />
                     <h3 >{this.props.date}</h3>
                     <h3 >{this.props.time}</h3>
                     <h3 >{this.props.location}</h3>
-                    <button className="btn btn-dark">See Details</button>
-
-                 <Link  to={`/EventMain`} >
-                </Link>
+                    <Link to="/EventDetail" role="button" className="btn btn-dark">See Details</Link>
+                 <Link  to={`/EventMain`} ></Link>
+                 </div>
+            </div>
             </div>
         )
     }
