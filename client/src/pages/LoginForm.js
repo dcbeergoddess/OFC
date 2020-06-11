c10deda7cbb8a44e5e392fdc9ec8c36100e96e4c
Bryan <bryan.fennell@hotmail.com>
2020-06-11 13:48:39 -0400

    Made logon and register forms responsive.

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -15,21 +15,21 @@ function LoginForm(props) {
         <h1 className="text-center mb-4">Log In</h1>
         <form className="form-group" onSubmit={props.onSubmit}>
           <div className="form-group row">
-            <label htmlFor="username" className="col-4 col-form-label">Username:</label>
+            <label htmlFor="username" className="col-12 col-md-4 col-form-label">Username:</label>
             <input
               name="username"
               type="text"
               autoComplete="off"
-              className="form-control col-8"
+              className="form-control col-12 col-md-8"
             />
           </div>
           <div className="form-group row">
-            <label htmlFor="password" className="col-4 col-form-label">Password:</label>
+            <label htmlFor="password" className="col-12 col-md-4 col-form-label">Password:</label>
             <input
               name="password"
               type="password"
               autoComplete="off"
-              className="form-control col-8"
+              className="form-control col-12 col-md-8"
             />
           </div>
           <button type="submit" className="btn btn-primary">Log In</button>
--- client/src/pages/RegisterForm.js
+++ client/src/pages/RegisterForm.js
@@ -29,31 +29,31 @@ function RegisterForm(props) {
         <h1 className="text-center mb-4">Register</h1>
         <form className="form-group" onSubmit={props.onSubmit} name='register'>
           <div className="form-group row">
-            <label htmlFor="username" className="col-4 col-form-label">Username:</label>
+            <label htmlFor="username" className="col-12 col-md-4 col-form-label">Username:</label>
             <input
               name="username"
               type="text"
               autoComplete="off"
-              className="form-control col-8"
+              className="form-control col-12 col-md-8"
             />
           </div>
           <div className="form-group row">
-            <label htmlFor="password" className="col-4 col-form-label">Password:</label>
+            <label htmlFor="password" className="col-12 col-md-4 col-form-label">Password:</label>
             <input
               name="password"
               type="password"
               autoComplete="off"
-              className="form-control col-8"
+              className="form-control col-12 col-md-8"
             />
           </div>
           <div className="form-group row">
-            <label htmlFor="confirm-password" className="col-4 col-form-label">Confirm Password:</label>
+            <label htmlFor="confirm-password" className="col-12 col-md-4 col-form-label">Confirm Password:</label>
             <input
               name="confirm-password"
               type="password"
               onChange={matchPasswords}
               autoComplete="off"
-              className="form-control col-8"
+              className="form-control col-12 col-md-8"
             />
           </div>
 
