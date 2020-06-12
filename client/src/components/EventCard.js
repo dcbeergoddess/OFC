028fed2897646273b1e4b1267bb429dd993287df
github username <github email address>
2020-06-12 16:37:24 -0400

    edited some cards to make them a bit more responsive; need to make some additions

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Flex} from "../components/Flex";
 import "./EventCard.css";
 import "../index.css";
 
@@ -8,15 +9,17 @@ class EventCard extends React.Component {
     render() {
         return (
             <div className="card">
-                <h1 className="name">{this.props.title}</h1>
-                <img src={this.props.image}
-                    alt={this.props.title} />
-                <h6 > <strong>Event Date: </strong>{this.props.date}</h6>
-                <h6 > <strong>Start Time: </strong>{this.props.time}</h6>
-                <h6 > <strong>Meetup Location:</strong> {this.props.location}</h6>
-                <h6><strong>Event Description: </strong>{this.props.description}</h6>
-                <Link to="/EventDetail" role="button" className="btn btn-dark">See Details</Link>
-                <Link to={`/EventMain`} ></Link>
+                <Flex>
+                    <h1 className="name">{this.props.title}</h1>
+                    <img src={this.props.image}
+                        alt={this.props.title} />
+                    <h6 > <strong>Event Date: </strong>{this.props.date}</h6>
+                    <h6 > <strong>Start Time: </strong>{this.props.time}</h6>
+                    <h6 > <strong>Meetup Location:</strong> {this.props.location}</h6>
+                    <h6><strong>Event Description: </strong>{this.props.description}</h6>
+                    <Link to="/EventDetail" role="button" className="btn btn-dark">See Details</Link>
+                    <Link to={`/EventMain`} ></Link>
+                </Flex>
             </div>
         )
 
