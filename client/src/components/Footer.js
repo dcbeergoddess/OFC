812dea762f2c203133d3df25452a00d6034acd61
Rachel Murray <dcbeergoddess@gmail.com>
2020-06-13 09:52:26 -0400

    Footer component created. Needs to be added and imported to App.js

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Organizing for Change &copy; 2020</span>
+    </footer>
+  );
+}
+
+export default Footer;
