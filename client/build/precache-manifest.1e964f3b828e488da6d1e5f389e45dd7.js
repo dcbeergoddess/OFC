cac090ced6ddfde6e1532df890b3047a59d7b7eb
Rachel Murray <dcbeergoddess@gmail.com>
2020-06-14 11:16:50 -0400

    troubleshooting heroku errors

---
+++ client/build/precache-manifest.1e964f3b828e488da6d1e5f389e45dd7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab1840cd2cf9d194b2b49c7b03a44e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0544bfeab60bd03aa7",
+    "url": "/static/css/2.c9b7eea9.chunk.css"
+  },
+  {
+    "revision": "c95e7bd88731c48bef7f",
+    "url": "/static/css/main.e0efb370.chunk.css"
+  },
+  {
+    "revision": "9a0544bfeab60bd03aa7",
+    "url": "/static/js/2.b42adc5b.chunk.js"
+  },
+  {
+    "revision": "1ac6cb7dd57c9567588e621817a5c5c3",
+    "url": "/static/js/2.b42adc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95e7bd88731c48bef7f",
+    "url": "/static/js/main.58e7e60b.chunk.js"
+  },
+  {
+    "revision": "317e369d82dfbf3c3424",
+    "url": "/static/js/runtime-main.cda93741.js"
+  },
+  {
+    "revision": "a8ca8e96bc5149a2607b8d1118cf704f",
+    "url": "/static/media/BLM_StreetSign.a8ca8e96.png"
+  },
+  {
+    "revision": "897a887e30fcd8f42f585c036dcd392a",
+    "url": "/static/media/Cornerstone.897a887e.ttf"
+  },
+  {
+    "revision": "724388969a9c08dde1c2537c9d724c8f",
+    "url": "/static/media/background.72438896.jpg"
+  }
+]);
