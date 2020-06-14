70fa564f6ec74b746168164f9be2f927858a17ec
Claudia Petty <claudialove32@gmail.com>
2020-06-14 10:06:20 -0400

    background color on card

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -11,12 +11,11 @@ class EventCard extends React.Component {
         return (
 
             <Flex container width="100%" flexDirection="column" justifyContent="space-around">
-                <div className="card">
+                <div className="card" style={{background: "#202020", color: "#FAFAD2", padding: "4rem", marginLeft: "16rem", marginRight:"16rem" }}>
                     <h1 className="name">{this.props.title}</h1>
                     <img src={this.props.image}
                         alt={this.props.title} />
-                    <h6 > <strong>Event Date: </strong>
-                        <Moment format="MM/DD/YYYY">{this.props.date}</Moment>
+                    <h6 > <strong>Event Date: </strong> 
                     </h6>
                     <h6 > <strong>Start Time: </strong>
                         <Moment format="h:mma">{this.props.date}</Moment>
