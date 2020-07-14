31c4355f9279b65b686e9ba54bc9df155fee0b61
Raymond Anuforo Jr <anufororaymond@yahoo.com>
2020-07-14 19:00:34 -0400

    updated TimelineCard.js for better flex design

--- client/src/components/TimelineCard/TimelineCard.js
+++ client/src/components/TimelineCard/TimelineCard.js
@@ -1,22 +1,48 @@
 import React from "react";
 import "./TimelineCard.css";
+import "../../index.css";
+import Moment from 'react-moment'
+import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
+
 
 function TimelineCard(props) {
   return (
-    <div className="card"style={{background: "#202020", color: "#FAFAD2"}}>
-      <h2>{props.title}</h2>
-      <h6>{props.date}</h6>
-      <div className="img-container">
-        <img 
-        alt={props.title}
-        src={props.image}
-         />
-      </div>
-      <div className="content">
-        <h6>{props.text}</h6>
-      </div>
-    </div>
-  );
-}
+//     <div className="card"style={{background: "#202020", color: "#FAFAD2"}}>
+//       <h2>{props.title}</h2>
+//       <h6>{props.date}</h6>
+//       <div className="img-container">
+//         <img 
+//         alt={props.title}
+//         src={props.image}
+//          />
+//       </div>
+//       <div className="content">
+//         <h6>{props.text}</h6>
+//       </div>
+//     </div>
+//   );
+// }
+
+// class TimelineCard extends React.Component {
+//   render() {
+//       return (
+         <>
+            <Card style={{ width: '50rem', background: "#202020", color: "#FAFAD2" }}>
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                </Card.Body>
+                <Card.Img variant="top" src={props.image} alt={props.title} />
+                <ListGroup style={{ background: "#202020", color: "#FAFAD2" }} className="list-group-flush">
+                    <ListGroupItem style={{ background: "#202020", color: "#FAFAD2" }}>
+                      {props.date}</ListGroupItem>
+                    <ListGroupItem>{props.text}</ListGroupItem>
+                </ListGroup>
+
+              </Card>
+              
+          </>
+        )
+
+    };
 
 export default TimelineCard;
